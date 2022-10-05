5c9cef124cc5446f9a9453c13c7c57f223504558
Baudes Matias <riplik1988@gmail.com>
2022-10-05 00:05:16 -0300

    añadia rama ayuda fix static

--- APPCAC/urls.py
+++ APPCAC/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from APPCAC.views import *
+from APPCAC.views import inicio,preguntas_frecuentes
 
 urlpatterns = [
     path('',inicio, name='Index'),
+    path('preguntas_frecuentes/',preguntas_frecuentes,name= 'APPCACfrecuentes')
 ]
--- APPCAC/views.py
+++ APPCAC/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'index.html')
-    
+
+def preguntas_frecuentes(request):
+    return render(request, 'ayuda.html')
+
+
 
