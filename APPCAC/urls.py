011baa798b4a795087ecd40dd9e7edd99a781f7d
Baudes Matias <riplik1988@gmail.com>
2022-10-10 19:16:01 -0300

    integrado ayuda al proyecto, pagina priincipal OK

--- APPCAC/urls.py
+++ APPCAC/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path,include
-from APPCAC.views import inicio,preguntas_frecuentes
+from APPCAC.views import inicio,preguntas_frecuentes,about_us,animation
 
 urlpatterns = [
     path('',inicio, name='Index'),
-    path('preguntas_frecuentes/',preguntas_frecuentes,name= 'APPCACfrecuentes')
+    path('preguntas_frecuentes/',preguntas_frecuentes,name= 'APPCACfrecuentes'),
+    path('about/',about_us, name='APPCACAboutus'),
+    path('animation/',animation, name='APPCACAnimation'),   
 ]
--- APPCAC/views.py
+++ APPCAC/views.py
@@ -6,5 +6,8 @@ def inicio(request):
 def preguntas_frecuentes(request):
     return render(request, 'ayuda.html')
 
+def about_us(request):
+    return render(request, 'about_us.html')
 
-
+def animation(request):
+    return render(request, 'animation.html')
